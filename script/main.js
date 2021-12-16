9fd8afcbcda64b8f3469e95af8bb85681d83db37
antoine <antoine.pizzetta@ynov.com>
2021-12-16 17:13:41 +0100

    resized splashes

--- script/main.js
+++ script/main.js
@@ -1,11 +1,8 @@
-// $('#DIVA').mouseenter(function() {
-//     $('#DIVB').show();
-// }).mouseleave(function() {
-//     if(!$('#DIVB').hasClass('active')){
-//         $('#DIVB').hide();
-//     }
-// });
-//
-// $('.characters').onmouseleave(function() {
-//     $('.')
-// })
+$(document).ready(function(){
+    $(".char_icon:hover + .characters").mouseenter(function(){
+        $(".characters").css("animation: rotateX 0.4s 180ms ease-in-out both;");
+    });
+    $("p").mouseleave(function(){
+        $("p").css("background-color", "lightgray");
+    });
+});
