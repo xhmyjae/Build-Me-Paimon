9d9b2c62088055e1c12ee9b08851235a4c0f5e6d
antoine <antoine.pizzetta@ynov.com>
2022-01-06 16:59:14 +0100

    loader index fixed + 0.5 beginning time

--- script/main.js
+++ script/main.js
@@ -5,8 +5,12 @@ function showCharacters(){
 
 
 window.addEventListener('load', () => {
+   const waiting = 0.5; //time waiting AFTER window is loaded to display
    const loader = document.querySelector('.loader-wrapper');
    loader.classList.add('fondu-out');
+   setTimeout(function(){
+      loader.style.zIndex = "1";
+   }, waiting * 1000); //time in second * 1000
 })
 
 
