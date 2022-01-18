cc31cd14448ff6c196995457c7261d4c6f485387
antoine <antoine.pizzetta@ynov.com>
2022-01-18 13:30:09 +0100

    loading screen fixed + anti scroll

--- script/main.js
+++ script/main.js
@@ -3,13 +3,25 @@ function showCharacters(){
    ctn.classList.toggle('open-characters');
 }
 
+function disableScrolling(){
+   var x=window.scrollX;
+   var y=window.scrollY;
+   window.onscroll=function(){window.scrollTo(x, y);};
+}
+
+function enableScrolling(){
+   window.onscroll=function(){};
+}
+
 
 window.addEventListener('load', () => {
+   disableScrolling()
    const waiting = 0.5; //time waiting AFTER window is loaded to display
    const loader = document.querySelector('.loader-wrapper');
    loader.classList.add('fondu-out');
    setTimeout(function(){
       loader.style.zIndex = "1";
+      enableScrolling()
    }, waiting * 1000); //time in second * 1000
 })
 
